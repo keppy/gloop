5e417c813a0f6a5e37c83009bdba7538d1a011ac
keppy <keppy.rb@gmail.com>
2022-03-07 13:52:57 -0800

    pointers for loops--list isn't going to cut it

--- gloop.go
+++ gloop.go
@@ -1,41 +1,52 @@
 package gloop
 
-import (
-    //"fmt"
-)
+import "fmt"
 
+// Loop interface for a game which has one main loop and, potentially,
+// many nested loops.
 type Loop interface {
-    tick() error
-    update(*State) error
+	tick() error
+	update(*State) error
 }
+
+// State structure holding the persistent data for a loop or a game.
 type State struct {
-    Epoch int
-    Geom  [][]int
-    Flags map[string]bool
+	Epoch int
+	Geom  [][]int
+	Flags map[string]bool
 }
+
 func (s *State) tick() error {
-    s.Epoch++
-    return nil
+	s.Epoch++
+	return nil
 }
 func (s *State) update(st *State) error {
-    if err := s.tick(); err != nil {
-        return err
-    }
-    s.Geom  = st.Geom
-    s.Flags = st.Flags
-    return nil
+	if err := s.tick(); err != nil {
+		return err
+	}
+	s.Geom = st.Geom
+	s.Flags = st.Flags
+	return nil
 }
 
-type game interface {
-    tickLoops() error
+// Game can control a list of Loops and dispatch actions against them.
+type Game interface {
+	TickLoops() error
 }
+
+// Gloop structure which inherits the functionality and storage of State, and
+// holds a list of States which are themselves Loops.
 type Gloop struct {
-    State
-    Loops []State
+	State
+	Loops []*State
 }
-func (g Gloop) tickLoops() error {
-    for _, s := range g.Loops {
-        s.tick()
-    }
-    return nil
+
+// TickLoops advances every epoch in Loops.
+func (g *Gloop) TickLoops() error {
+	fmt.Println("pre: ", g.Loops)
+	for _, s := range g.Loops {
+		s.tick()
+	}
+	fmt.Println("post: ", g.Loops)
+	return nil
 }
--- gloop_test.go
+++ gloop_test.go
@@ -1,59 +1,98 @@
 package gloop
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 
-    "github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestState(t *testing.T) {
-    s := State{Epoch: 0}
-    s.tick()
-    if s.Epoch != 1 {
-        t.Errorf("Epoch = %d, want 1", s.Epoch)
-    }
-    s.tick()
-    if s.Epoch != 2 {
-        t.Errorf("Epoch = %d, want 2", s.Epoch)
-    }
+	s := State{Epoch: 0}
+	s.tick()
+	if s.Epoch != 1 {
+		t.Errorf("Epoch = %d, want 1", s.Epoch)
+	}
+	s.tick()
+	if s.Epoch != 2 {
+		t.Errorf("Epoch = %d, want 2", s.Epoch)
+	}
 }
 
 func TestUpdate(t *testing.T) {
-    var tests = []struct {
-        state State
-        input State
-        want State
-    }{
-        {
-            State{0, [][]int{}, map[string]bool{}},
-            State{
-                4,
-                [][]int{
-                    {1,2,3},
-                    {4,5,6},
-                },
-                map[string]bool{"wookies": true},
-            },
-            State{
-                1,
-                [][]int{
-                    {1,2,3},
-                    {4,5,6},
-                },
-                map[string]bool{"wookies": true},
-            },
-        },
-    }
-    for n, tt := range tests {
-        m := fmt.Sprintf("Epoch:test= %d %d", tt.state.Epoch, n)
-        t.Run(m, func(t *testing.T) {
-            if err := tt.state.update(&tt.input); err != nil {
-                t.Errorf("update error: %s", err)
-            }
-            if diff := cmp.Diff(tt.state, tt.want); diff != "" {
-                t.Errorf("update() mismatch (-want +got):\n%s", diff)
-            }
-        })
-    }
+	var tests = []struct {
+		state State
+		input State
+		want  State
+	}{
+		{
+			State{0, [][]int{}, map[string]bool{}},
+			State{
+				4,
+				[][]int{
+					{1, 2, 3},
+					{4, 5, 6},
+				},
+				map[string]bool{"wookies": true},
+			},
+			State{
+				1,
+				[][]int{
+					{1, 2, 3},
+					{4, 5, 6},
+				},
+				map[string]bool{"wookies": true},
+			},
+		},
+	}
+	for n, tt := range tests {
+		m := fmt.Sprintf("Epoch:test= %d %d", tt.state.Epoch, n)
+		t.Run(m, func(t *testing.T) {
+			if err := tt.state.update(&tt.input); err != nil {
+				t.Errorf("update() error: %s", err)
+			}
+			if diff := cmp.Diff(tt.state, tt.want); diff != "" {
+				t.Errorf("update() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestTickLoops(t *testing.T) {
+	var tests = []struct {
+		have Gloop
+		want Gloop
+	}{
+		{
+			Gloop{
+				State: State{0, [][]int{}, map[string]bool{}},
+				Loops: []*State{
+					&State{0, [][]int{}, map[string]bool{}},
+					&State{1, [][]int{}, map[string]bool{}},
+					&State{0, [][]int{}, map[string]bool{}},
+				},
+			},
+			Gloop{
+				State: State{0, [][]int{}, map[string]bool{}},
+				Loops: []*State{
+					&State{1, [][]int{}, map[string]bool{}},
+					&State{2, [][]int{}, map[string]bool{}},
+					&State{1, [][]int{}, map[string]bool{}},
+				},
+			},
+		},
+	}
+	for n, tt := range tests {
+		m := fmt.Sprintf("Epoch:test= %d %d", tt.have.Epoch, n)
+		t.Run(m, func(t *testing.T) {
+			fmt.Println("before: ", tt.have.Loops)
+			if err := tt.have.TickLoops(); err != nil {
+				t.Errorf("TickLoops() error: %s", err)
+			}
+			fmt.Println("after: ", tt.have.Loops)
+			if diff := cmp.Diff(tt.have.Loops, tt.want.Loops); diff != "" {
+				t.Errorf("TickLoops() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
 }
